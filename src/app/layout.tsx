7b9e30c9ebf6dc2349436e1e62faac8fcb4c5472
neinja007 <anton.siligan@gmail.com>
2024-08-17 11:34:42 +0200

    add other files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,24 @@ import { Rubik } from 'next/font/google';
 const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'PROJECT NAME',
-	description: 'PROJECT DESCRIPTION'
+  title: 'PROJECT NAME',
+  description: 'PROJECT DESCRIPTION'
 };
 const RootLayout = ({
-	children
+  children
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) => {
-	return (
-		<html lang='en'>
-			<body className={rubik.className}>
-				<div className='min-h-screen pb-16 pt-24'>
-					{/* <Navbar /> */}
-					<div className='container mx-auto'>{children}</div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <body className={rubik.className}>
+        <div className='min-h-screen pb-16 pt-24'>
+          {/* <Navbar /> */}
+          <div className='container mx-auto'>{children}</div>
+        </div>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
